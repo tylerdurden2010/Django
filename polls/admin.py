380627e935c287a043ba349d1850df57c649759a
Tylerdurdn <tylerdurdn2010@gmail.com>
2013-03-27 18:23:00 +0800

    第二章

---
+++ polls/admin.py
@@ -0,0 +1,11 @@
+from polls.models import Poll
+from django.contrib import admin
+from polls.models import Choice
+class PollAdmin(admin.ModelAdmin):
+    fieldsets = [
+                    (None,      {'fields':['question']}),
+                    ('Date information',      {'fields':['pub_date'],'classes':['collapse']}),
+                ]
+
+admin.site.register(Poll,PollAdmin)
+admin.site.register(Choice)
