00bc51df9f0180a304c507b179af13511455d388
Tylerdurdn <tylerdurdn2010@gmail.com>
2013-03-29 16:58:40 +0800

    Added 3 choice for default

--- polls/admin.py
+++ polls/admin.py
@@ -1,11 +1,16 @@
 from polls.models import Poll
 from django.contrib import admin
 from polls.models import Choice
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
 class PollAdmin(admin.ModelAdmin):
     fieldsets = [
                     (None,      {'fields':['question']}),
                     ('Date information',      {'fields':['pub_date'],'classes':['collapse']}),
                 ]
-
+    inlines = [ChoiceInline]
 admin.site.register(Poll,PollAdmin)
-admin.site.register(Choice)
+#admin.site.register(Choice)
