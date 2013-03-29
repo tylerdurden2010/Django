b4e22cf464e34408718507515ee3f47b74d3a04f
Tylerdurdn <tylerdurdn2010@gmail.com>
2013-03-29 17:02:06 +0800

    Describe how to work for list_display
    I has modified the show line

--- polls/admin.py
+++ polls/admin.py
@@ -1,12 +1,13 @@
 from polls.models import Poll
 from django.contrib import admin
 from polls.models import Choice
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
 
 class PollAdmin(admin.ModelAdmin):
+    list_display = ('question','pub_date','was_published_recently')
     fieldsets = [
                     (None,      {'fields':['question']}),
                     ('Date information',      {'fields':['pub_date'],'classes':['collapse']}),
